8ee3a9c4dddff5dd1dfdb4ae037f1cc8cd2f709f
jirayut cc <jirayut_ssww@hotmail.com>
2024-01-01 17:29:23 +0700

    added Validator

---
+++ src/main/java/com/spring/development/utils/Validator.java
@@ -0,0 +1,34 @@
+package com.spring.development.utils;
+
+import java.util.List;
+
+public class Validator {
+
+	public static boolean isEmpty(String inputStr) {
+		return null == inputStr
+			|| inputStr.trim().length() == 0
+			|| inputStr.equalsIgnoreCase("null");
+	}
+
+	public static boolean isNotEmpty(String inputStr) {
+		return null != inputStr
+			&& inputStr.trim().length() > 0
+			&& !inputStr.equalsIgnoreCase("null");
+	}
+
+	public static boolean isEmpty(List<?> inputList) {
+		return null == inputList || inputList.isEmpty();
+	}
+
+	public static boolean isNotEmpty(List<?> inputList) {
+		return null != inputList && !inputList.isEmpty();
+	}
+
+	public static boolean isNull(Object object) {
+		return null == object || object.equals("null");
+	}
+
+	public static boolean isNotNull(Object object) {
+		return null != object && !object.equals("");
+	}
+}
