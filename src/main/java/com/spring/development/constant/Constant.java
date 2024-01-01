837284ab60530c7fd7ba8a2f3fce3c615930dfee
jirayut cc <jirayut_ssww@hotmail.com>
2024-01-01 17:29:06 +0700

    updated Constant

--- src/main/java/com/spring/development/constant/Constant.java
+++ src/main/java/com/spring/development/constant/Constant.java
@@ -2,19 +2,14 @@ package com.spring.development.constant;
 
 public class Constant {
 
-    public static class DateTimeFormat {
-        public static final String DATE_TIME_FORMAT_WITH_SPACE = "yyyy-MM-dd HH:mm:ss.SSS";
-        public static final String DATE_TIME_FORMAT_ISO = "yyyy-MM-dd'T'HH:mm:ss.SSS";
-        private DateTimeFormat() {}
-    }
+	private Constant() {
+	}
 
-    public enum Action {
-        CREATE,
-        UPDATE,
-        DELETE
-    }
-    public enum Flag {
-        Y,
-        N
-    }
+	public static class DateTimeFormat {
+		public static final String DATE_TIME_FORMAT_WITH_SPACE = "yyyy-MM-dd HH:mm:ss.SSS";
+		public static final String DATE_TIME_FORMAT_ISO = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+
+		private DateTimeFormat() {
+		}
+	}
 }
