0b829d449cc6e7dff877f0c4bb7eefcddf842581
jirayut cc <jirayut_ssww@hotmail.com>
2024-01-04 22:37:59 +0700

    added ApplicationController

---
+++ src/main/java/com/spring/development/controller/ApplicationController.java
@@ -0,0 +1,31 @@
+package com.spring.development.controller;
+
+import com.spring.development.dto.ApplicationRequest;
+import com.spring.development.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/application")
+@RequiredArgsConstructor
+public class ApplicationController {
+
+	private final ApplicationService versionService;
+
+	@GetMapping("/version")
+	public ResponseEntity<?> version(
+		@RequestHeader MultiValueMap<String, String> httpHeaders,
+		@RequestBody ApplicationRequest request
+	) {
+		var response = versionService.version(httpHeaders, request);
+
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+}
