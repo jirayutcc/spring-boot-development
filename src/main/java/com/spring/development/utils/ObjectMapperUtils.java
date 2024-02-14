f18360fa9015ae88a1e1429ddc09d9db8e2df7a1
jirayut cc <jirayut_ssww@hotmail.com>
2024-02-14 11:09:49 +0700

    updated ObjectMapperUtils

--- src/main/java/com/spring/development/utils/ObjectMapperUtils.java
+++ src/main/java/com/spring/development/utils/ObjectMapperUtils.java
@@ -8,10 +8,12 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.spring.development.constant.Constant;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class ObjectMapperUtils {
 
 	public static final ObjectMapper staticObjectMapper = getObjectMapper();
